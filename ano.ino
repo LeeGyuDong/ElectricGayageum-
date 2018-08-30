8e0dba27f80a8ea3eca793e26fcef25ec9d35ba3
LeeKyuDong <dlrbehd06@naver.com>
2018-08-31 01:48:38 +0900

    final

---
+++ ano.ino
@@ -0,0 +1,160 @@
+#include <SoftwareSerial.h>
+#include <DFPlayer_Mini_Mp3.h>
+#include <Wire.h>
+#include "Adafruit_MPR121.h"
+
+void setup();
+void loop();
+
+uint8_t touch_flag = 1;
+uint8_t press_flag = 0;
+uint8_t mp3_flag = 1;
+
+uint8_t current_mp3 = -1;
+
+int r0 = 0;
+int r1 = 0;
+int r2 = 0;
+
+int vias = 1;
+
+Adafruit_MPR121 cap = Adafruit_MPR121();
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
+
+void setup() {
+  Serial.begin(9600);    
+  //SoftwareSerial mySerial(10, 11); // RX, TX
+
+  while (!Serial)
+  { // needed to keep leonardo/micro from starting too fast!
+      delay(10);
+  }
+
+  Serial.println("initialize start");
+
+   //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ터치 센서 초기화ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+  if(touch_flag > 0)
+  {
+    Serial.println("Adafruit_MPR121 end");
+    if (!cap.begin(0x5A))
+    {
+      Serial.println("MPR121 not found, check wiring?");
+    }
+    Serial.println("MPR121 found!");
+  }
+
+  Serial.println("df init start");
+  //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡdf 플레이어 초기화ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+  if(mp3_flag > 0)
+  {
+    //  mp3_set_serial (mySerial);
+    mp3_set_serial(Serial); // DFPlayer-mini mp3 module 시리얼 세팅
+    delay(1);               // 볼륨값 적용을 위한 delay
+    mp3_set_volume(30);     // 볼륨조절 값 0~30
+  }
+
+  
+  Serial.println("initialize complete");
+}
+
+void loop() {
+
+   if(press_flag > 0)
+   {
+      r0 = analogRead(A0);
+      r1 = analogRead(A1);
+      r2 = analogRead(A2);
+      
+      //if(r0 != 0)
+      {
+        //Serial.print("a0 :");
+       //Serial.print(r0);
+        //Serial.print("\n"); 
+      }
+  
+      //if(r1 != 0)
+      {
+       // Serial.print("a1 :");
+        //Serial.print(r1);
+        //Serial.print("\n");
+      }
+       //if(r2 != 0)
+      {
+        //Serial.print("a2 :");
+        //Serial.print(r2);
+        //Serial.print("\n");
+      }
+   }
+
+  
+   if(touch_flag > 0)
+   {
+     currtouched = cap.touched();
+
+      for (uint8_t i=0; i<12; i++)
+      {
+        if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) )
+        {          
+          Serial.print(i); Serial.println(" touched");
+          if(mp3_flag > 0)
+          {
+            if( current_mp3 != i)
+            {
+              Serial.print("play\n");
+              current_mp3 = i;
+              
+              if(press_flag > 0)
+              {
+                
+                if( i <= 2) //r0 센서
+                {
+                  if(r0 <= 10)
+                    vias = 1;
+                  else if( r0 <= 50 )
+                    vias = 3;
+                  else
+                    vias = 6;
+                }
+                else if( i <= 6) //r1 센서
+                {
+                  if(r1 <= 20)
+                    vias = 1;
+                  else if( r1 <= 23 )
+                    vias = 3;
+                  else
+                    vias = 6;
+                }
+                else if( i <= 10) //r2 센서
+                {
+                  if(r2 <= 0)
+                    vias = 1;
+                  else if( r2 <= 2 )
+                    vias = 3;
+                  else
+                    vias = 6;
+                }   
+                    
+              }
+             
+
+              mp3_play (17 - (current_mp3 / 2 * 3 + vias) ); 
+              
+            }
+            
+          }
+        }
+        if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) )
+        {
+          Serial.print(i); Serial.println(" released");
+          current_mp3  = -1;
+        }
+      }
+
+      lasttouched = currtouched;
+   }
+
+    
+
+   
+}
---
+++ sub2.ino
@@ -0,0 +1,415 @@
+﻿
+#include<LiquidCrystal.h>
+
+#define led1 12 //중
+#define led2 3 //임
+#define led3 4 //무
+#define led4 7 //황
+#define led5 8 //태
+#define led6 9 //높중
+
+int step=0;
+int btn1=5; //1 
+int btn2=6; //2
+
+ 
+int state1=0;
+int state2=0;
+
+LiquidCrystal lcd(3,4,10,11,12,13);
+
+
+
+void tone1(int time);
+void tone2(int time);
+void tone3(int time);
+void tone4(int time);
+void tone5(int time);
+void tone6(int time);
+
+
+void tone1(int time){
+  int t=time;
+  digitalWrite(led1, HIGH);
+  Serial.println("중");
+   delay(t-100);
+   digitalWrite(led1, LOW);
+   delay(100);
+ }
+void tone2(int time){
+  int t=time;
+  digitalWrite(led2, HIGH);
+  Serial.println("임");
+  delay(t-100);//2
+  digitalWrite(led2, LOW);
+  delay(100);
+ }
+ void tone3(int time){
+  int t=time;
+  digitalWrite(led3, HIGH);
+  Serial.println("무");
+  delay(t-100);//1
+  digitalWrite(led3, LOW);
+   delay(100);
+ }
+void tone4(int time){
+  int t=time;
+  digitalWrite(led4, HIGH);
+  Serial.println("황");
+  delay(t-100);//1
+  digitalWrite(led4, LOW);
+  delay(100);
+ }
+ void tone5(int time){
+    int t=time;
+    digitalWrite(led5, HIGH);
+    Serial.println("태");
+   delay(t-100);//1
+   digitalWrite(led5, LOW);
+   delay(100);
+ }
+  void tone6(int time){
+    int t=time;
+    digitalWrite(led6, HIGH);
+    Serial.println("높중");
+   delay(t-100);//1
+   digitalWrite(led6, LOW);
+   delay(100);
+ }
+
+
+void song1(){
+ lcd.clear();
+   lcd.setCursor(0,0);
+    lcd.print("Playing Song1");
+  Serial.println("song1<아리랑>: 시작!!");
+   digitalWrite(led1,LOW);
+    digitalWrite(led2,LOW);
+    digitalWrite(led3,LOW);
+     digitalWrite(led4,LOW);
+    digitalWrite(led5,LOW);
+    digitalWrite(led6,LOW);
+    delay(2000);
+  tone1(1500); 
+  tone2(500);
+  tone1(500);
+  tone2(500);
+
+  tone3(1500);
+  tone4(500);
+  tone3(500);
+  tone4(500);
+
+  tone5(1000);
+  tone4(500);
+  tone5(500);
+  tone3(500);
+  tone2(500);
+
+  tone1(1500);
+  tone2(500);
+  tone1(1000);
+
+  tone3(1500);
+  tone4(500);
+  tone3(500);
+  tone4(500);
+
+  tone5(500);
+  tone4(500);
+  tone3(500);
+  tone2(500);
+  tone1(500);
+  tone2(500);
+
+  tone3(1500);
+  tone4(500);
+  tone3(1000);
+
+  tone3(3000);
+
+  tone6(2000);
+  tone6(1000);
+
+  tone6(1000);
+  tone5(1000);
+  tone4(1000);
+
+  tone5(1000);
+  tone4(500);
+  tone5(500);
+  tone3(500);
+  tone2(500);
+
+  tone1(1500);
+  tone2(500);
+  tone1(1000);
+
+  tone3(1500);
+  tone4(500);
+  tone3(500);
+  tone4(500);
+
+  tone5(500);
+  tone4(500);
+  tone3(500);
+  tone2(500);
+  tone1(500);
+  tone2(500);
+
+  tone3(1500);
+  tone4(500);
+  tone3(1000);
+
+  tone3(3000);
+    digitalWrite(led1,HIGH);
+    digitalWrite(led2,HIGH);
+    digitalWrite(led3,HIGH);
+    digitalWrite(led4,HIGH);
+    digitalWrite(led5,HIGH);
+    digitalWrite(led6,HIGH);
+    delay(1000);
+    digitalWrite(led1,LOW);
+    digitalWrite(led2,LOW);
+    digitalWrite(led3,LOW);
+    digitalWrite(led4,LOW);
+    digitalWrite(led5,LOW);
+    digitalWrite(led6,LOW);
+    
+    Serial.println("song1 clear");
+      lcd.clear();
+  lcd.print("set");
+  delay(2000);
+  setup();
+}
+
+
+void song2(){
+  
+ lcd.clear();
+   lcd.setCursor(0,0);
+    lcd.print("Playing Song2");
+  Serial.println("song2<홀로 아리랑>: 시작!!");
+/////////
+  digitalWrite(led1,LOW);
+    digitalWrite(led2,LOW);
+    digitalWrite(led3,LOW);
+     digitalWrite(led4,LOW);
+    digitalWrite(led5,LOW);
+    digitalWrite(led6,LOW);
+    delay(2000);
+    tone1(500);
+    tone2(500); 
+    tone3(1000);
+    tone3(1000);
+
+    tone4(500);
+    tone5(500); 
+    tone5(1000);
+    tone4(1000);
+
+    tone3(1000);
+    tone4(500);
+    tone3(500);
+    tone2(250);
+    tone1(250);
+    tone2(500);
+
+    tone1(2000);
+    delay(1000);
+
+    tone5(500);
+    tone6(500);
+    tone6(1000);
+    tone6(1000);
+    
+    tone5(500);
+    tone5(500);
+    tone4(1000);
+    tone3(1000);
+    
+    tone5(500);
+    tone5(500);
+    tone4(1000);
+    tone3(500);
+    tone4(500);
+
+    tone4(2000);
+    delay(1000);
+
+    tone6(1000);
+    tone6(1000);
+    tone6(1000);
+    
+    tone5(1000);
+    tone4(1000);
+    tone3(1000);
+
+    tone5(1000);
+    tone4(500);
+    tone3(500);
+    tone2(250);
+    tone1(250);
+    tone2(500);
+
+    tone1(2000);
+    delay(1000);
+
+    tone3(1000);
+    tone3(1000);
+    tone4(1000);
+
+    tone5(1000);
+    tone4(1000);
+    tone3(1000);
+
+    tone2(1500);
+    tone1(500);
+    tone2(500);
+    tone3(500);
+
+    tone3(2000);
+    delay(1000);
+        
+//////////////
+  
+    digitalWrite(led1,HIGH);
+    digitalWrite(led2,HIGH);
+    digitalWrite(led3,HIGH);
+    digitalWrite(led4,HIGH);
+    digitalWrite(led5,HIGH);
+    digitalWrite(led6,HIGH);
+    delay(1000);
+    digitalWrite(led1,LOW);
+    digitalWrite(led2,LOW);
+    digitalWrite(led3,LOW);
+    digitalWrite(led4,LOW);
+    digitalWrite(led5,LOW);
+    digitalWrite(led6,LOW);
+    
+    Serial.println("song2 clear");
+  lcd.clear();
+  lcd.print("set");
+  delay(2000);
+  setup();
+}
+
+
+void ready_song1(){
+  lcd.clear();
+   lcd.setCursor(0,0);
+    lcd.print("Song1");
+    
+   lcd.setCursor(0,1);
+    lcd.print("1)Play");
+    step=2;
+  }
+
+  void ready_song2(){
+  lcd.clear();
+   lcd.setCursor(0,0);
+    lcd.print("Song2");
+    
+   lcd.setCursor(0,1);
+    lcd.print("1)Play");
+    step=2;
+  }
+
+void SelectSong(){
+  
+    lcd.setCursor(0,0);
+    lcd.print("Select Song");
+    lcd.setCursor(0,1);
+    lcd.print("1)SONG1 2)SONG2");
+    step=1;
+
+  }
+void setup() {
+  // put your setup code here, to run once:
+  step=0;
+   Serial.begin(9600);
+   Serial.println("start");
+    state1=HIGH;
+  state2=HIGH;
+     pinMode(btn1,INPUT_PULLUP);
+   pinMode(btn2, INPUT_PULLUP);
+  
+
+   pinMode(led1, OUTPUT);
+   pinMode(led2, OUTPUT);
+   pinMode(led3, OUTPUT);
+   pinMode(led4, OUTPUT);
+   pinMode(led5, OUTPUT);
+   pinMode(led6, OUTPUT);
+  
+  lcd.begin(16,2);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Game start");
+  
+    digitalWrite(led1,HIGH);
+    digitalWrite(led2,HIGH);
+    digitalWrite(led3,HIGH);
+    digitalWrite(led4,HIGH);
+    digitalWrite(led5,HIGH);
+    digitalWrite(led6,HIGH);
+    delay(2000);
+ 
+
+    
+   Serial.println("loopstart");
+}
+
+void loop() {
+ 
+
+   state1=HIGH;
+    state2=HIGH;
+     
+  state1 = digitalRead(btn1);
+  state2 = digitalRead(btn2);    
+    
+  if(state1 ==LOW&&step==0){
+    SelectSong();
+    
+   Serial.println(step);
+   state1=HIGH;
+    state2=HIGH;
+    delay(2000);
+  } 
+  
+   state1 = digitalRead(btn1);
+  state2 = digitalRead(btn2);    
+  if(state1 ==LOW&&step==1){
+   delay(1000);
+    ready_song1();
+     state1=HIGH;
+    state2=HIGH;
+    delay(2000);
+  }
+
+  else if(state2 ==LOW&&step==1){
+    delay(1000);
+    ready_song2();
+     state1=HIGH;
+    state2=HIGH;
+    delay(2000);
+    }
+   state1 = digitalRead(btn1);
+  state2 = digitalRead(btn2);    
+    
+
+  if(state1 ==LOW&&step==2){
+   delay(1000);
+  song1();
+  }
+  else if(state2 ==LOW&&step==2){
+   delay(1000);
+  song2();
+  }
+
+ 
+  //delay(10000);
+// lcd.clear();
+  
+}
